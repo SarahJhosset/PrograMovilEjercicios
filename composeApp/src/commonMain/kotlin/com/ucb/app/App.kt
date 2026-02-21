d6e90136cac506ce9ab8f0570065fa407bca6943
Roberto Callisaya <calyr.software@gmail.com>
2026-02-21 09:18:33 -0400

    MVI

--- composeApp/src/commonMain/kotlin/com/ucb/app/App.kt
+++ composeApp/src/commonMain/kotlin/com/ucb/app/App.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
@@ -14,23 +16,24 @@ import com.ucb.app.counter.presentation.viewmodel.CounterViewModel
 import com.ucb.app.detail.presentation.screen.DetailScreen
 import com.ucb.app.increment.presentation.screen.IncrementScreen
 import com.ucb.app.increment.presentation.viewmodel.IncrementViewModel
+import com.ucb.app.nm.login.presentation.screen.LoginScreen
 import com.ucb.app.product_detail.presentation.screen.ProductDetailScreen
+import com.ucb.app.signin.presentation.screen.SigninScreen
 
 
 @Composable
 @Preview
 fun App() {
+    val snackbarHostState = remember { SnackbarHostState() }
     MaterialTheme(
-        colorScheme = darkColorScheme()
+        //colorScheme = darkColorScheme()
     ) {
         Scaffold(
-            contentWindowInsets = WindowInsets.safeDrawing
+            contentWindowInsets = WindowInsets.safeDrawing,
+            snackbarHost = { SnackbarHost(snackbarHostState) }
         ) { paddingVaues ->
-//            CollectionScreen( modifier = Modifier.padding(paddingVaues))
-            //DetailScreen(modifier = Modifier.padding(paddingVaues))
-//            ProductDetailScreen(modifier = Modifier.padding(paddingVaues))
-//            ProductDetailScreen( modifier = Modifier.padding(paddingVaues))
-            IncrementScreen()
+
+            SigninScreen()
         }
 
     }
--- composeApp/src/commonMain/kotlin/com/ucb/app/di/PresentationModule.kt
+++ composeApp/src/commonMain/kotlin/com/ucb/app/di/PresentationModule.kt
@@ -3,6 +3,7 @@ package com.ucb.app.di
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.ucb.app.counter.presentation.viewmodel.CounterViewModel
 import com.ucb.app.increment.presentation.viewmodel.IncrementViewModel
+import com.ucb.app.nm.login.presentation.viewmodel.LoginViewModel
 import com.ucb.app.product_detail.presentation.screen.ProductDetailScreen
 import com.ucb.app.product_detail.presentation.viewmodel.ProductDetailViewModel
 import org.koin.core.module.dsl.viewModelOf
@@ -14,4 +15,5 @@ val presentationModule = module {
         ::ProductDetailViewModel)
     viewModelOf(::CounterViewModel)
     viewModelOf(::IncrementViewModel)
+    viewModelOf(::LoginViewModel)
 }
